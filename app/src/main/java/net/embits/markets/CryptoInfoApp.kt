41ec69cad700c6072212629d8961fdb30d0abbe8
Anton Forosenko <anton.forosenko@gmail.com>
2023-10-19 15:02:41 +0300

    Initial commit

---
+++ app/src/main/java/net/embits/markets/CryptoInfoApp.kt
@@ -0,0 +1,8 @@
+package net.embits.markets
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class CryptoInfoApp : Application() {
+}
---
+++ app/src/main/java/net/embits/markets/MainActivity.kt
@@ -0,0 +1,10 @@
+package net.embits.markets
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
